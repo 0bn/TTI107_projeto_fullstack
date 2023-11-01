6d54992492ead73d4eccc369b6e94565abf1d66f
marcos pedro <70784710+0bn@users.noreply.github.com>
2023-11-01 12:35:48 -0300

    01/11

--- front/js/script.js
+++ front/js/script.js
@@ -1,10 +1,20 @@
-const protocolo = 'https//';
+const protocolo = 'http://';
 const host = 'localhost:3000';
 const filmesEndPoint = '/filmes';
 
 async function obterFilmes() {
-    console.log("Teste 123");
+    //console.log("Teste 123");
     const URLcompleta = `${protocolo}${host}${filmesEndPoint}`;
+    console.log (URLcompleta);
     const filmes = (await axios.get(URLcompleta)).data;
-    console.log(filmes);
+    //console.log(filmes);
+    let tabela = document.querySelector('.filmes');
+    let corpoTabela = tabela.getElementsByTagName('tbody')[0];
+    for(let filme of filmes) {
+        let linha = corpoTabela.insertRow(0);
+        let celulaTitulo = linha.insertCell(0);
+        let celulaSinopse = linha.insertCell(1);
+        celulaTitulo.innerHTML = filme.celulaTitulo;
+        celulaSinopse.innerHTML = filme.celulaSinopse;
+    }
 }
