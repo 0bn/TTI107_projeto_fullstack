babc162ef371d8807ac744f4eec3bc26308ff439
marcos pedro <70784710+0bn@users.noreply.github.com>
2023-11-08 12:44:00 -0300

    08/11

---
+++ README.md
@@ -0,0 +1,3 @@
+# Exc-full-stack
+Aula de Front-End 26/10
+Exercio em aula full-stack
--- front/js/script.js
+++ front/js/script.js
@@ -1,20 +1,56 @@
-const protocolo = 'http://';
-const host = 'localhost:3000';
-const filmesEndPoint = '/filmes';
+const protocolo = 'http://'
+const host = 'localhost:3000'
+const filmesEndpoint = '/filmes'
 
 async function obterFilmes() {
-    //console.log("Teste 123");
-    const URLcompleta = `${protocolo}${host}${filmesEndPoint}`;
-    console.log (URLcompleta);
+    // console.log("teste123")
+    const URLcompleta = `${protocolo}${host}${filmesEndpoint}`;
     const filmes = (await axios.get(URLcompleta)).data;
-    //console.log(filmes);
+    //console.log(filmes)
     let tabela = document.querySelector('.filmes');
     let corpoTabela = tabela.getElementsByTagName('tbody')[0];
-    for(let filme of filmes) {
-        let linha = corpoTabela.insertRow(0);
-        let celulaTitulo = linha.insertCell(0);
-        let celulaSinopse = linha.insertCell(1);
-        celulaTitulo.innerHTML = filme.celulaTitulo;
-        celulaSinopse.innerHTML = filme.celulaSinopse;
+    for (let filme of filmes) {
+        let linha = corpoTabela.insertRow(0)
+        let celulaTitulo = linha.insertCell(0)
+        let celulaSinopse = linha.insertCell(1)
+        celulaTitulo.innerHTML = filme.titulo
+        celulaSinopse.innerHTML = filme.sinopse
     }
+}
+async function CadastrarFilme() {
+    const URLcompleta = `${protocolo}${host}${filmesEndpoint}`;
+    let tituloInput = document.querySelector("#tituloInput");
+    let sinopseInput = document.querySelector("#sinopseInput");
+    let titulo = tituloInput.value;
+    let sinopse = sinopseInput.value;
+    tituloInput.value = "";
+    sinopseInput.value = "";
+    if(titulo && sinopse) {
+        tituloInput.value = ""
+        sinopseInput.value = ""
+        const filmes = (await axios.post(URLcompleta, {titulo,sinopse})).data
+
+        let tabela = document.querySelector('.filmes')
+        let corpoTabela = tabela.getElementsByTagName('tbody')[0]
+        corpoTabela.innerHTML = ""
+
+        for (let filme of filmes) {
+            let linha = corpoTabela.insertRow(0)
+            let celulaTitulo = linha.insertCell(0)
+            let celulaSinopse = linha.insertCell(1)
+            celulaTitulo.innerHTML = filme.titulo
+            celulaSinopse.innerHTML = filme.sinopse
+        }
+    }
+    
+else{
+        let alert = document.querySelector('.alert');
+        alert.classList.add('show');
+        alert.classList.remove('d-nome');
+        setTimeout(() => {
+            alert.classList.add('d-none');
+            alert.classList.remove('show');
+        }, 2000);
+    }
+
 }
